62eb424ccbb0f8f6377a6488e50620163b4eb224
RuscalWorld <me@ruscalworld.ru>
2021-01-05 23:17:33 +0300

    Экономика, предметы

---
+++ src/main/java/ru/ruscalworld/bortexel4j/models/economy/Item.java
@@ -0,0 +1,70 @@
+package ru.ruscalworld.bortexel4j.models.economy;
+
+import com.google.gson.reflect.TypeToken;
+import ru.ruscalworld.bortexel4j.Bortexel4J;
+import ru.ruscalworld.bortexel4j.core.Action;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public class Item {
+    private final String id;
+    private String name;
+    private String category;
+
+    public Item(String id, String name, String category) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+    }
+
+    public static Action<Item> getByID(String id) {
+        Action<Item> action = new Action<>("/economy/items/" + id, new Bortexel4J());
+        action.setResponseType(Item.class);
+        return action;
+    }
+
+    public Action<List<ItemPrice>> getPrices() {
+        Action<List<ItemPrice>> action = new Action<>("/economy/items/" + id + "/prices", new Bortexel4J());
+        action.setResponseType(TypeToken.getParameterized(List.class, ItemPrice.class).getType());
+        return action;
+    }
+
+    public static class ItemPrice {
+        private final Timestamp time;
+        private final float price;
+
+        public ItemPrice(Timestamp time, float price) {
+            this.time = time;
+            this.price = price;
+        }
+
+        public Timestamp getTime() {
+            return time;
+        }
+
+        public float getPrice() {
+            return price;
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
---
+++ src/test/java/ru/ruscalworld/bortexel4j/models/economy/ItemTest.java
@@ -0,0 +1,26 @@
+package ru.ruscalworld.bortexel4j.models.economy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+    @Test
+    void getByID() {
+        Item item = Item.getByID("diamond_axe").execute();
+        assertNotNull(item);
+        assertEquals("diamond_axe", item.getId());
+    }
+
+    @Test
+    void getPrices() {
+        Item item = Item.getByID("diamond_axe").execute();
+        assertNotNull(item);
+
+        List<Item.ItemPrice> prices = item.getPrices().execute();
+        assertNotNull(prices);
+        assertNotEquals(0, prices.size());
+    }
+}
