4230216541ce8f2859f84ae7990c7091651fe4fe
RuscalWorld <me@ruscalworld.ru>
2021-02-07 15:44:45 +0300

    Убраны тесты для сервера авторизации

--- src/test/java/ru/ruscalworld/bortexel4j/auth/SessionTest.java
+++ src/test/java/ru/ruscalworld/bortexel4j/auth/SessionTest.java
@@ -1,14 +1,11 @@
 package ru.ruscalworld.bortexel4j.auth;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import ru.ruscalworld.bortexel4j.AuthClient;
 import ru.ruscalworld.bortexel4j.exceptions.LoginException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SessionTest {
-    @Test
     void loginByName() {
         assertThrows(LoginException.class, () -> Session.loginByName("RuscalWorld", "What did you expect to see here?..", new AuthClient()).execute());
     }
