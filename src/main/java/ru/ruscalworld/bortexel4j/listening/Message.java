e99359d8ef2ef5ad57e2df12d6c02aa1e778a507
RuscalWorld <me@ruscalworld.ru>
2021-03-30 22:50:53 +0300

    Поддержка типа предупреждения при обработке событий

--- src/main/java/ru/ruscalworld/bortexel4j/listening/Message.java
+++ src/main/java/ru/ruscalworld/bortexel4j/listening/Message.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonElement;
 import okhttp3.WebSocket;
 import ru.ruscalworld.bortexel4j.listening.events.Event;
 import ru.ruscalworld.bortexel4j.models.ban.Ban;
+import ru.ruscalworld.bortexel4j.models.warning.Warning;
 
 import java.lang.reflect.Type;
 
@@ -30,6 +31,10 @@ public class Message {
             case Event.BAN_UPDATED_EVENT:
             case Event.BAN_DELETED_EVENT:
                 return Ban.class;
+            case Event.WARNING_CREATED_EVENT:
+            case Event.WARNING_UPDATED_EVENT:
+            case Event.WARNING_DELETED_EVENT:
+                return Warning.class;
         }
 
         return Object.class;
