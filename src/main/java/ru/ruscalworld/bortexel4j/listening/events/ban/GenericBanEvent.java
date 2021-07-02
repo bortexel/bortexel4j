4378e5355f8376b39e072f40c153ff7421dec275
RuscalWorld <me@ruscalworld.ru>
2021-07-02 15:30:19 +0300

    Обновлены схемы собитий в соответствии с изменениями в API

--- src/main/java/ru/ruscalworld/bortexel4j/listening/events/ban/GenericBanEvent.java
+++ src/main/java/ru/ruscalworld/bortexel4j/listening/events/ban/GenericBanEvent.java
@@ -3,12 +3,22 @@ package ru.ruscalworld.bortexel4j.listening.events.ban;
 import ru.ruscalworld.bortexel4j.listening.events.Event;
 import ru.ruscalworld.bortexel4j.models.ban.Ban;
 
-public class GenericBanEvent extends Event<Ban> {
+public class GenericBanEvent extends Event<GenericBanEvent> {
+    private Ban ban;
+
     public GenericBanEvent(Event<Object> event) {
-        super(event.getEventID(), (Ban) event.getPayload());
+        super(event.getEventID(), (GenericBanEvent) event.getPayload());
     }
 
-    public GenericBanEvent(int eventID, Ban payload) {
+    public GenericBanEvent(int eventID, GenericBanEvent payload) {
         super(eventID, payload);
+    }
+
+    public Ban getBan() {
+        return ban;
+    }
+
+    protected void setBan(Ban ban) {
+        this.ban = ban;
     }
 }
--- src/main/java/ru/ruscalworld/bortexel4j/listening/events/warning/GenericWarningEvent.java
+++ src/main/java/ru/ruscalworld/bortexel4j/listening/events/warning/GenericWarningEvent.java
@@ -3,12 +3,22 @@ package ru.ruscalworld.bortexel4j.listening.events.warning;
 import ru.ruscalworld.bortexel4j.listening.events.Event;
 import ru.ruscalworld.bortexel4j.models.warning.Warning;
 
-public class GenericWarningEvent extends Event<Warning> {
+public class GenericWarningEvent extends Event<GenericWarningEvent> {
+    private Warning warning;
+
     public GenericWarningEvent(Event<Object> event) {
-        super(event.getEventID(), (Warning) event.getPayload());
+        super(event.getEventID(), (GenericWarningEvent) event.getPayload());
     }
 
-    public GenericWarningEvent(int eventID, Warning payload) {
+    public GenericWarningEvent(int eventID, GenericWarningEvent payload) {
         super(eventID, payload);
+    }
+
+    public Warning getWarning() {
+        return warning;
+    }
+
+    protected void setWarning(Warning warning) {
+        this.warning = warning;
     }
 }
