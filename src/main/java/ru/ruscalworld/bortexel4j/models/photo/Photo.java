24eb0527c189aeef7a83652511a312d4028ca623
RuscalWorld <me@ruscalworld.ru>
2021-10-05 18:46:31 +0300

    Поддержка прокси для изображений

--- src/main/java/ru/ruscalworld/bortexel4j/models/photo/Photo.java
+++ src/main/java/ru/ruscalworld/bortexel4j/models/photo/Photo.java
@@ -1,6 +1,7 @@
 package ru.ruscalworld.bortexel4j.models.photo;
 
 import com.google.gson.annotations.SerializedName;
+import org.jetbrains.annotations.Nullable;
 import ru.ruscalworld.bortexel4j.Bortexel4J;
 import ru.ruscalworld.bortexel4j.core.Action;
 import ru.ruscalworld.bortexel4j.core.PaginatedListAction;
@@ -9,38 +10,77 @@ public class Photo {
     private final int id;
     private String url;
     private String description;
-
     @SerializedName(value = "author_name")
     private String authorName;
-
     private int season;
+    @Nullable
+    private final Proxy proxy;
 
-    public Photo(int id, String url, String description, String author, int season) {
+    public Photo(int id, String url, String description, String author, int season, @Nullable Proxy proxy) {
         this.id = id;
         this.url = url;
         this.description = description;
         this.authorName = author;
         this.season = season;
+        this.proxy = proxy;
     }
 
     public static Action<Photo> getByID(int id) {
-        return getByID(id, new Bortexel4J());
+        return getByID(id, Bortexel4J.anonymous());
     }
 
     public static Action<Photo> getByID(int id, Bortexel4J client) {
-        Action<Photo> action = new Action<>("/photos/" + id, client);
+        return getByID(id, false, client);
+    }
+
+    public static Action<Photo> getByID(int id, boolean proxy, Bortexel4J client) {
+        Action<Photo> action = new Action<>("/photos/" + id + "?proxy=" + proxy, client);
         action.setResponseType(Photo.class);
         return action;
     }
 
     public static PaginatedListAction<Photo> getAll() {
-        return getAll(new Bortexel4J());
+        return getAll(Bortexel4J.anonymous());
     }
 
     public static PaginatedListAction<Photo> getAll(Bortexel4J client) {
-        PaginatedListAction<Photo> action = new PaginatedListAction<>("/photos", client);
+        return getAll(false, client);
+    }
+
+    public static PaginatedListAction<Photo> getAll(boolean proxy, Bortexel4J client) {
+        PaginatedListAction<Photo> action = new PaginatedListAction<>("/photos?proxy=" + proxy, client);
         action.setResponseListType(Photo.class);
         return action;
+    }
+
+    public static class Proxy {
+        private final String originalURL;
+        private final String smallURL;
+        private final String mediumURL;
+        private final String largeURL;
+
+        public Proxy(String originalURL, String smallURL, String mediumURL, String largeURL) {
+            this.originalURL = originalURL;
+            this.smallURL = smallURL;
+            this.mediumURL = mediumURL;
+            this.largeURL = largeURL;
+        }
+
+        public String getOriginalURL() {
+            return originalURL;
+        }
+
+        public String getSmallURL() {
+            return smallURL;
+        }
+
+        public String getMediumURL() {
+            return mediumURL;
+        }
+
+        public String getLargeURL() {
+            return largeURL;
+        }
     }
 
     public int getID() {
@@ -77,5 +117,9 @@ public class Photo {
 
     public void setSeason(int season) {
         this.season = season;
+    }
+
+    public @Nullable Proxy getProxy() {
+        return proxy;
     }
 }
--- src/test/java/ru/ruscalworld/bortexel4j/models/photo/PhotoTest.java
+++ src/test/java/ru/ruscalworld/bortexel4j/models/photo/PhotoTest.java
@@ -1,6 +1,7 @@
 package ru.ruscalworld.bortexel4j.models.photo;
 
 import org.junit.jupiter.api.Test;
+import ru.ruscalworld.bortexel4j.Bortexel4J;
 
 import java.util.List;
 
@@ -18,5 +19,13 @@ class PhotoTest {
     void getAll() {
         List<Photo> photos = Photo.getAll().execute();
         assertNotEquals(0, photos.size());
+    }
+
+    @Test
+    void getProxy() {
+        Photo photo = Photo.getByID(2, true, Bortexel4J.anonymous()).execute();
+        assertNotNull(photo);
+        assertNotNull(photo.getProxy());
+        assertNotNull(photo.getProxy().getOriginalURL());
     }
 }
