c9e00cc1ca220ae8477f27a9fe9264a4f86a0af7
RuscalWorld <me@ruscalworld.ru>
2021-10-15 23:10:32 +0300

    Не проверять тип контента при коде HTTP 204

--- src/main/java/ru/ruscalworld/bortexel4j/util/APIUtil.java
+++ src/main/java/ru/ruscalworld/bortexel4j/util/APIUtil.java
@@ -4,8 +4,9 @@ import java.util.Objects;
 
 public class APIUtil {
     public static void checkResponse(okhttp3.Response apiResponse) throws RuntimeException {
-        if (apiResponse.header("Content-Type") == null) throw new RuntimeException("API server has returned invalid Content-Type header");
-        if (!Objects.requireNonNull(apiResponse.header("Content-Type")).equalsIgnoreCase("application/json"))
+        if (apiResponse.code() != 204 && apiResponse.header("Content-Type") == null)
+            throw new RuntimeException("API server has returned invalid Content-Type header");
+        if (apiResponse.code() != 204 && !Objects.requireNonNull(apiResponse.header("Content-Type")).equalsIgnoreCase("application/json"))
             throw new RuntimeException("API server has returned invalid Content-Type header");
         if (apiResponse.body() == null) throw new RuntimeException("Body of API response is empty");
     }
